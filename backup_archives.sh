aed762f84dda227814f7d858069f6af774227b3f
Justineneema <j.neema@alustudent.com>
2024-10-14 13:02:39 +0200

    create archived_logs_group27 and move archive_log.sh

---
+++ backup_archives.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This script creates a directory named 'archived_logs_group$'
+mkdir archived_logs_group27
+
+# Move archived log files to the archived_logs_group27 directory
+mv archive_log.sh archived_logs_group27
+
+
