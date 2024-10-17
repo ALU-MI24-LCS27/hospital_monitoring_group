4eb6f0eca254e6352efef38aba440fdb55a085f3
BYUSAA <byuussaa@gmail.com>
2024-10-17 13:56:33 +0100

    updated backup-arc

--- backup_archives.sh
+++ backup_archives.sh
@@ -6,4 +6,4 @@ mkdir archived_logs_group27
 mv heart_rate_log.txt archived_logs_group27
 
 # Set up SSH connection to remote server
-ssh "ff613cff72d1@ff613cff72d1.601a9382.alu-cod.online"
+scp "ff613cff72d1@ff613cff72d1.601a9382.alu-cod.online" archived_logs_group27:/home/
